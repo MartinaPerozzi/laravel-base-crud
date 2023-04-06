3204dff4865e5f1f65985aa14d917460e1f57982
MartinaPerozzi <perozzimartina@gmail.com>
2023-04-07 00:27:00 +0200

    +added small style

--- resources/views/songs/show.blade.php
+++ resources/views/songs/show.blade.php
@@ -1,15 +1,18 @@
 @include('partials.head')
 
 <div class="container mt-4">
-    <strong>Title: </strong> {{ $song->title }} <br />
-    <strong>Album:</strong> {{ $song->album }} <br />
-    <strong>Author: </strong> {{ $song->author }} <br />
-    <strong>Editor: </strong> {{ $song->editor }} <br />
-    <strong>Length: </strong> {{ $song->length }} <br />
-    <div class="img-container">
-        <strong>Poster: </strong>
-        <img src="{{ $song->poster }}" alt="Poster">
+    <div class="container-show">
+        <strong>Title: </strong> {{ $song->title }} <br />
+        <strong>Album:</strong> {{ $song->album }} <br />
+        <strong>Author: </strong> {{ $song->author }} <br />
+        <strong>Editor: </strong> {{ $song->editor }} <br />
+        <strong>Length: </strong> {{ $song->length }} <br />
+        <div class="img-container">
+            <strong>Poster: </strong>
+            <img src="{{ $song->poster }}" alt="Poster">
+        </div>
     </div>
 
     <a href="{{ route('songs.index') }}" role="button" class="btn btn-primary mt-3"> Torna alla home </a>
+    <a href="{{ route('songs.edit', $song) }}" role="button" class="btn btn-primary mt-3"> Modifica </a>
 </div>
