db7308843a6497ac6bef7a611241c7295519d83e
MartinaPerozzi <perozzimartina@gmail.com>
2023-04-06 16:50:56 +0200

    + added seeder and faker

---
+++ database/migrations/2023_04_06_131648_create_songs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title', 80);
+            $table->string('album', 60)->default('Unknown');
+            $table->string('author', 50)->default('Unknown');
+            $table->string('editor', 50)->nullable();
+            $table->float('length', 15);
+            $table->text('poster')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('songs');
+    }
+};
---
+++ database/seeders/SongSeeder.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Song;
+
+class SongSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $song = new Song;
+
+        $song->title = 'Bohemian Rhapsody';
+        $song->album = 'A Night at the Opera';
+        $song->author = 'Queen';
+        $song->editor = 'Roy Thomas Baker';
+        $song->length = 09.00;
+        $song->poster = "https://upload.wikimedia.org/wikipedia/it/8/8b/BohemianRhapsody.jpg";
+
+        $song->save();
+
+        $song = new Song;
+
+        $song->title = 'For Now I Am Winter';
+        $song->album = 'For Now I Am Winter';
+        $song->author = 'Ólafur Arnalds';
+        $song->editor = 'Erased Tapes Records';
+        $song->length = 05.09;
+        $song->poster = "https://picsum.photos/200/200";
+
+        $song->save();
+
+        for ($i = 0; $i < 20; $i++) {
+
+            $song = new Song;
+
+            $song->title = $faker->word();
+            $song->album = $faker->word();
+            $song->author = $faker->firstName();
+            $song->editor = $faker->company();
+            $song->length = $faker->numberBetween(30, 2000);
+            $song->poster = "https://picsum.photos/200/200";
+
+            $song->save();
+        };
+    }
+}
